70f7181681ca38101b6257b90b29e56930caf2a2
SamukaMaciell <122841270+SamukaMaciell@users.noreply.github.com>
2023-01-24 00:26:03 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function altenarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+     altenarModal();
+     video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    altenarModal();
+    video.setAttribute("src", "");
+});
+
